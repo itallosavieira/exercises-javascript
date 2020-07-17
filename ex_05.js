b9977a4d8504239ac5e06d439fa4f744dec8a6b3
itallosavieira <itallo.sa.vieira@gmail.com>
2020-07-16 23:49:40 -0300

    update

--- ex_05.js
+++ ex_05.js
@@ -8,35 +8,29 @@ const database = {
     createTable(statement) {
         const regexp = /create table (\w+) \((.+)\)/;
         const parsedStatement = statement.match(regexp);
-        const tableName = parsedStatement[1];
+        let [,tableName, columns] = parsedStatement;
         this.tables[tableName] = {
             columns: {},
             data: []
-        };
-        let columns = parsedStatement[2];
+        };    
         columns = columns.split(", ");
         for (column of columns) {
             column = column.split(" ");
-            let name = column[0];
-            let type = column[1];
+            const [name, type] = column;
             this.tables[tableName].columns[name] = type;
         }
     },
     insert(statement) {
         const regexp = /insert into (\w+) \((.+)\) values \((.+)\)/;
         const parsedStatement = statement.match(regexp);
-        const tableName = parsedStatement[1];
-        this.tables[tableName] = {
-            columns: {},
-            data: []
-        };
-        let columns = parsedStatement[2];
+        let [,tableName, columns, values] = parsedStatement;
         columns = columns.split(", ");
-        let values = parsedStatement[3];
         values = values.split(", ");
+        let row = {};
         for (let i = 0; i < columns.length; i++) {
-            
-        }
+            row[columns[i]] = values[i];
+        };
+        this.tables[tableName].data.push(row);
     },
     execute(statement) {
         if (statement.startsWith("create table")) {
@@ -45,7 +39,7 @@ const database = {
         if (statement.startsWith("insert into")) {
             return this.insert(statement);
         }
-        const message = `Syntax error: "${statement}"`;
+        const message = `Syntax Error: "${statement}"`;
         throw new DatabaseError(statement, message);
     }
 };
@@ -54,10 +48,8 @@ try {
     database.execute("create table author (id number, name string, age number, city string, state string, country string)");
     database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
     database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");            
-    //console.log(JSON.stringify(database));
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+    console.log(JSON.stringify(database, undefined, " "));
 } catch(e) {
     console.log(e.message);
-};
-
-
+}
